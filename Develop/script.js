78b2123eb0f07f0b43585ba45640c5327ebf895c
hannahk <hanhannah839@gmail.com>
2022-10-15 12:43:56 -0700

    final javascript

--- Develop/script.js
+++ Develop/script.js
@@ -22,58 +22,53 @@ function getRandomInt(max) {
   return Math.ceil(Math.random() * max)
 }
 
-
 // Function to generate password
 function generatePassword () {
  // Step 1 Ask if how many characters does the user want the password to be?
-  var passwordLength = parseInt(prompt("how many characters would you like your password to be (8-128)", "12"))
+  var passwordLength = parseInt(prompt("How many characters would you like your password to be? must be (8-128)", "12"))
     console.log(passwordLength);
     if (!passwordLength || passwordLength<8 || passwordLength>128) {
       alert("The length of your password must be greater than 8 characters and less than 128 characters.")
       return generatePassword ()
     }
   // Step 2 Ask if the user wants to use uppercase
-  var useUpperCase = confirm("does the user want to use uppercase letter?")
+  var useUpperCase = confirm("Would you like to use uppercase letters?")
     console.log(useUpperCase);
   // Step 3 Ask if the user wants to use lowercase
-  var useLowercase = confirm("does the user want to use lowercase letter?")
+  var useLowercase = confirm("Would you like to use lowercase letters?")
     console.log(useLowercase);
   // Step 4 Ask if the user wants to use numbers
-  var useNumbers = confirm("does the user want to use numbers?")
+  var useNumbers = confirm("Would you like to use numbers?")
     console.log(useNumbers);
   // Step 5 Ask if the user wants to use special characters
-  var specialCaseChar = confirm("does the user want to use special chararacters?")
+  var specialCaseChar = confirm("Would you like to use special characters?")
     console.log(specialCaseChar);
 
+  // Create the structure of potential characters
   var possibleChar = []
     if (useUpperCase){
-      possibleChar = possibleChar.concat(uppercase)
-       
+      possibleChar = possibleChar.concat(uppercase)  
     }
-    // 3 more if statements for the other character types
 
+    if (useLowercase){
+      possibleChar = possibleChar.concat(lowercase)
+    }
+  
+    if (useNumbers){
+      possibleChar = possibleChar.concat(numbers)
+    }
 
+    if (specialCaseChar){  
+      possibleChar = possibleChar.concat(special)
+    }
+
+  // Create the password of "passwordLength" length
   var generatedPassword = ""
     for (var i = 0; i < passwordLength; i++) { 
       var randomIndex = Math.floor(Math.random()* possibleChar.length)
       var randomChar = possibleChar[randomIndex]
       generatedPassword += randomChar
-
     }
 
-    return generatedPassword
-  
+  return generatedPassword
 }
- 
-  // Arrange the proper characters and letters and numbers into a data structure that we can extract 
-  // A random character for each number of the length
-  // Create the structure of potential characters
-
-  // Create the password of "passwordLength" length
-
-
-  function Add (num1, num2) {
-    return num1 + num2
-  }
- var number = 22 
-  console.log(Add(number, 14))
